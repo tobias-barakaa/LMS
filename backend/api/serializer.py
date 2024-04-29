c56f13d573aff374268a35c6a2421b7363c403ca
tobias-barakaa <ngairato@gmail.com>
2024-04-29 11:39:06 +0300

    validate data

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from userauths.models import Profile, User
 
@@ -13,6 +14,32 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
         
         return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = User
+        fields = ['full_name', 'email', 'password', 'password2']
+        
+    def validate(self, attr):
+        if attr['password'] != attr['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match"})
+        return attr
+    
+    def create(self, validated_data):
+        user = User.objects.create (
+            full_name = validated_data['full_name'],
+            email = validated_data['email']
+        )
+        email_username, _ = user.email.split('@')
+        user.username = email_username
+        user.set_password(validated_data['password'])
+        user.save()
+        
+        return user
+    
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from api.serializer import MyTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from userauths.models import User
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
